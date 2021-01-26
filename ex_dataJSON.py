b7b10d13c123eb20c15a293cc61dc78624e2f5c3
Melanie Panem <melanielynnpanem@gmail.com>
2021-01-26 08:36:51 -0800

    finally finished with the JSON extraction and sum thanks to IRL dev problem solver StackOverflow

--- ex_dataJSON.py
+++ ex_dataJSON.py
@@ -2,7 +2,7 @@ import json
 import urllib.request
 
 # download raw json object
-url = "http://py4e-data.dr-chuck.net/comments_42.json"
+url = "http://py4e-data.dr-chuck.net/comments_1137003.json"
 
 # declare variables
 sum = 0
@@ -16,25 +16,34 @@ if len(dataLocate) < 1 : dataLocate = url
 # grabbing JSON data from URL page
 data = urllib.request.urlopen(dataLocate).read().decode()
 
-# parsing JSON object
-obj = json.loads(data)
-
 # print URL pulling from
 print('⭐', dataLocate)
 
 # print characters in data
 print('Retrieved', len(data), 'characters')
 
+# function to search for JSON key and grab its value
+def find_values(id, json_repr):
+    results = []
 
-print(obj)
-print(data)
+    def _decode_dict(a_dict):
+        try:
+            results.append(a_dict[id])
+        except KeyError:
+            pass
+        return a_dict
 
-# for item in obj:
-#     print('hmm', item['count'])
-# for item in data:
-#     print(item['count'])
+    json.loads(json_repr, object_hook=_decode_dict)
+    return results
 
+# using above function to create a for loop
+# iterate through each instance of the count key
+# + add instance to the total counter + add key value to the sum total
+for eachCount in find_values('count', data) :
+    count += 1
+    sum += eachCount
 
-# print(" Count:", count)
-
-# print(" Sum:", sum, '\n')
+# print counter
+print(" Count:", count)
+# print sum
+print(" Sum:", sum, '\n')
